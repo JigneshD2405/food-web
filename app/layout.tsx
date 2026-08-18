5947ad57269234b739f237ffc99fc4faf96833ea
jignesh-dudharejiya <jignesh.dudharejiya@team.mightytech.io>
2026-08-18 15:59:30 +0530

    feat: update typography in global styles; add font-family and modify body color; enhance Home component with header text

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import { Rubik } from "next/font/google";
 import "./globals.css";
-const rubik = Rubik({ variable: "--font-rubik", subsets: ["latin"] });
+// const rubik = Rubik({ variable: "--font-rubik", subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -8,7 +7,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${rubik.className} h-full antialiased`}>
+    // <html lang="en" className={`${rubik.className} h-full antialiased`}>
+    <html lang="en" className={`h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1></h1>
+      <h1>A healthy meal delivered to your door, every single day</h1>
     </>
   );
 }
